980d8ed1232b5d7e9e2f1464705d7f75709ae15c
Ravij Soni <ravijsoni@outlook.com>
2021-04-05 14:10:17 -0700

    104. Maximum Depth of Binary Tree

---
+++ MaximumDepthofBinaryTree.cs
@@ -0,0 +1,21 @@
+using System;
+  
+  public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+public class Solution {
+    public int MaxDepth(TreeNode root) {
+        if(root == null) return 0;
+        var l = MaxDepth(root.left);
+        var r = MaxDepth(root.right);
+        return Math.Max(l, r) + 1;
+    }
+}
